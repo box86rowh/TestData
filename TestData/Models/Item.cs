4efd3b25900f6dedc74871906660b3a9d62386fb
Jason Candelora <jasonatsmtc@hotmail.com>
2017-11-18 14:48:55 -0500

    refactor

--- TestData/Models/Item.cs
+++ TestData/Models/Item.cs
@@ -1,10 +1,6 @@
 ﻿using Dapper.Contrib.Extensions;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestData.Models
 {
