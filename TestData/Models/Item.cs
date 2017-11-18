b493abdd2a8a129f2c678c7ac7e4a55f7b61db87
Jason Candelora <jasonatsmtc@hotmail.com>
2017-11-18 14:36:06 -0500

    initial commit

---
+++ TestData/Models/Item.cs
@@ -0,0 +1,41 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData.Models
+{
+    [Table("AA_Items")]
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [Computed]
+        public string FullInfo
+        {
+            get
+            {
+                //any logic in here required to get the data
+                return Title + " " + Description;
+            }
+            set { }
+        }
+
+        public static IEnumerable<Item> GetAll(IDbConnection db)
+        {
+            return db.GetAll<Item>();
+        }
+
+        public static Item GetById(IDbConnection db, int id)
+        {
+            return db.Get<Item>(id);
+        }
+    }
+}
---
+++ TestData/Utils/DbUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData.Utils
+{
+    public static class DbUtil
+    {
+        public static IDbConnection GetConnection()
+        {
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SiteSqlServer"].ConnectionString);
+            return conn;
+        }
+    }
+}
